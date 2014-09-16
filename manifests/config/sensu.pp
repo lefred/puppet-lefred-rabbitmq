2f842c09e792b2c995632c9b2c73e0eecbaf593a
frederic.descamps <frederic.descamps@percona.com>
2014-09-16 22:18:18 +0200

    fix path again
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/config/sensu.pp
+++ manifests/config/sensu.pp
@@ -6,19 +6,19 @@ class rabbitmq::config::sensu ($rabbitmq_sensu_password="fred") {
         command   => "rabbitmqctl add_vhost /sensu",
         logoutput => true,
         unless    => "rabbitmqctl list_vhosts | grep '/sensu' > /dev/null",
-        path      => '/usr/sbin:/usr/local/sbin:/usr/bin:/usr/local/bin',
+        path      => '/usr/sbin:/bin:/usr/local/sbin:/usr/bin:/usr/local/bin',
         require   => Class['rabbitmq::config'];
     'add_user_sensu':
         command   => "rabbitmqctl add_user sensu $rabbitmq_sensu_password",
         logoutput => true,
         unless    => "rabbitmqctl list_users | grep 'sensu' > /dev/null",
-        path      => '/usr/sbin:/usr/local/sbin:/usr/bin:/usr/local/bin',
+        path      => '/usr/sbin:/bin:/usr/local/sbin:/usr/bin:/usr/local/bin',
         require   => Exec['add_vhost_sensu'];
     'set_permission_sensu':
         command   => 'rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"',
         logoutput => true,
         unless    => "rabbitmqctl list_permissions -p /sensu | grep 'sensu' > /dev/null",
-        path      => '/usr/sbin:/usr/local/sbin:/usr/bin:/usr/local/bin',
+        path      => '/usr/sbin:/bin:/usr/local/sbin:/usr/bin:/usr/local/bin',
         require   => Exec["add_user_sensu"];
  }
  
