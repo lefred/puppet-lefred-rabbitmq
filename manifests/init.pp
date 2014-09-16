c631113c7caf4eceaf54123cf75677bdc49ff5a7
frederic.descamps <frederic.descamps@percona.com>
2014-09-16 22:13:06 +0200

    stupid dependency loop
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class rabbitmq {
 	include rabbitmq::config
 	include rabbitmq::service
 	
-	Class['rabbitmq::packages'] -> Class['rabbitmq::config'] ->  Class['rabbitmq::service'] -> Class['rabbitmq::config']
+	Class['rabbitmq::packages'] -> Class['rabbitmq::config'] ->  Class['rabbitmq::service']
    
 
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -5,8 +5,8 @@ class rabbitmq::service{
             enable    => true,
             name      => $rabbitmq::packages::rabbitmq_server_bin,
             ensure    => running,
-            #require   => Package[$rabbitmq::packages::packs],
-            #subscribe => File["/etc/rabbitmq/rabbitmq.config"] 
+            require   => Package[$rabbitmq::packages::packs],
+            subscribe => File["/etc/rabbitmq/rabbitmq.config"] 
   }
   
 }
